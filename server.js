e8eca2d238a4140084a201c923167b3a43da4bd6
MatheusFelyppeDonato <41203236+MatheusFelyppeDonato@users.noreply.github.com>
2019-01-09 13:05:01 -0200

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@
   	res.status(200).send({ message: 'Hello world, I`m working'});
   })
 
+  app.get('/ola', (req, res) => {
+  res.status(200).send({ message: 'Hello, I am Olá`s rote'});
+  })
+
   app.listen(3001,  () => {
   	console.log('Api is roaming on 3001`s door');
   })
